1ba9347f2c7fd789077fb8a5cd99a716287db3bd
Haris Shah <harisshah385@gmail.com>
2022-12-08 12:20:46 +0500

    Add daisyUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
 export default function App() {
-  return <div className="text-8xl text-green-500">Suforia</div>;
+  return (
+    <div className="text-8xl text-green-500">
+      <h1>Suforia</h1>
+      <button className="btn btn-primary" type="submit">
+        Click
+      </button>
+    </div>
+  );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,5 +7,30 @@ module.exports = {
       sans: ['Poppins', 'sans-serif']
     }
   },
-  plugins: []
+  plugins: [require('daisyui')],
+  // daisyUI config (optional)
+  daisyui: {
+    themes: [
+      {
+        suforiaLight: {
+          primary: '#1a1a1a',
+          secondary: '#aaaaaa',
+          accent: '#708090',
+          neutral: '#f5f5f5',
+          info: '#199ee9',
+          success: '#15CF74',
+          warning: '#FBBD23',
+          error: '#ea4335'
+        }
+      }
+    ],
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: '',
+    darkTheme: 'dark'
+  }
 };
