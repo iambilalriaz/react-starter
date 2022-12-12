fc10ba1944a3acc6f31a6dbf7f927b558755a1df
Haris Shah <harisshah385@gmail.com>
2022-12-12 17:48:34 +0500

    Add button story in storybook

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import { ReactNode } from 'react';
 
-interface IButtonProps {
+export interface IButtonProps {
   children: ReactNode;
   // eslint-disable-next-line react/require-default-props
   type?: 'submit' | 'reset' | 'button';
@@ -9,7 +9,7 @@ interface IButtonProps {
   size?: 'sm' | 'lg';
 }
 
-export default function Button({ children, type = 'button', size }: IButtonProps): JSX.Element {
+export function Button({ children, type = 'button', size }: IButtonProps): JSX.Element {
   return (
     // eslint-disable-next-line react/button-has-type
     <button className={`btn bg-primary capitalize ${size ? 'w-full' : ''}`} type={type}>
