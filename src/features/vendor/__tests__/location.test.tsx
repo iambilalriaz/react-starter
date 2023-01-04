eb5c34d7c8eef8893761c0221501332008880183
Haris Shah <harisshah385@gmail.com>
2023-01-04 11:22:15 +0500

    Add Vitest

---
+++ src/features/vendor/__tests__/location.test.tsx
@@ -0,0 +1,8 @@
+import { test } from 'vitest';
+import { render } from '@testing-library/react';
+import { ViewLocations } from '../components/ViewLocations';
+
+test('just render the component', async () => {
+  // const viewLocations =
+  render(<ViewLocations />);
+});
